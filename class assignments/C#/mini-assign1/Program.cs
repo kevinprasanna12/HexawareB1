a600a7c603b034428b1e2080e31892a1b20d223b
Kevin Prasanna <kevindecr10@gmail.com>
2025-03-28 14:16:17 +0530

    class assignment update

---
+++ class assignments/C#/mini-assign1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+
+namespace Sample_pro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student stud_obj = new Student();
+
+            stud_obj.get_student_details();
+            stud_obj.display_student_details();
+
+        }
+    }
+}
---
+++ class assignments/C#/mini-assign1/student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_pro
+{
+    internal class Student
+    {
+        int student_id;
+        string name;
+        int age;
+        string department;
+    
+    public  void get_student_details()
+        {
+            Console.Write("Enter student ID: ");
+            student_id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter department: ");
+            department = Console.ReadLine();
+
+
+        }
+        public  void display_student_details()
+        {
+            Console.WriteLine("\nstudent details:");
+            Console.WriteLine($"ID: {student_id}");
+            Console.WriteLine($"name: {name}");
+            Console.WriteLine($"age: {age}");
+            Console.WriteLine($"department: {department}");
+        }
+    }
+}
